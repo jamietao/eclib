8aecd3ab9a4b75dafa2bb26f5235144824167ab5
Jamie Tao <jamie.tao@perkinelmer.com>
2019-05-30 17:43:33 +0800

    Initial commit for passbook service

---
+++ passbook-service/src/main/java/tech/yhao/eclib/passbook/PassbookApplication.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package tech.yhao.eclib.passbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author TaoJn
+ *
+ */
+@SpringBootApplication
+public class PassbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassbookApplication.class, args);
+	}
+}
